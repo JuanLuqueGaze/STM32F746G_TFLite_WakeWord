477e7aa9e393391b1c11ac281bc7246c5c1dca05
JuanLuque <juanluquegiron12@gmail.com>
2025-03-26 09:57:51 +0100

    Create a program that manages the UART print. This will help to print the output in the UART.

---
+++ Core/debug_log.c
@@ -0,0 +1,32 @@
+/**
+  ******************************************************************************
+  * @file    debug_log.cpp
+  * @author  Fahad Mirza
+  * @brief   This file provides TensorFLow DebugLog() implementation
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "tensorflow/lite/micro/debug_log.h"
+
+#include <stdio.h>
+
+
+#include "stm32f7xx_hal.h"
+#include "stm32f7xx_hal_uart.h"
+
+extern UART_HandleTypeDef DebugUartHandler;
+
+
+int __io_putchar(int ch)
+{
+	HAL_UART_Transmit(&DebugUartHandler, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+    return ch;
+}
+
+
+// Used by TFLite error_reporter
+void DebugLog(const char *s)
+{
+	fprintf(stderr, "%s", s);
+}
