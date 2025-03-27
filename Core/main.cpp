7209555f667a8154ae527ac05d92a6d8e9a17f21
JuanLuque <juanluquegiron12@gmail.com>
2025-03-27 10:13:07 +0100

    Added LCD function in the main.cpp file to print the results in the LCD screen.

--- Core/main.cpp
+++ Core/main.cpp
@@ -21,6 +21,7 @@
 #include "string.h"
 #include "cmsis_os.h"
 #include "fatfs.h"
+#include "lcd.h"
 #include "usb_host.h"
 #include "stm32746g_discovery.h"
 //Includes for the tensorflow code
@@ -153,6 +154,8 @@ int main(void)
   /* Initialize all configured peripherals */
   uart1_init();
 
+  LCD_Init();
+
     // Set up logging. Google style is to avoid globals or statics because of
   // lifetime uncertainty, but since this has a trivial destructor it's okay.
   // NOLINTNEXTLINE(runtime-global-variables)
@@ -225,7 +228,52 @@ int main(void)
 
   while (1)
   {
+      // Fetch the spectrogram for the current time.
+  const int32_t current_time = LatestAudioTimestamp();
+  int how_many_new_slices = 0;
+  TfLiteStatus feature_status = feature_provider->PopulateFeatureData(
+      error_reporter, previous_time, current_time, &how_many_new_slices);
+  if (feature_status != kTfLiteOk) {
+    error_reporter->Report("Feature generation failed");
+    return;
   }
+  previous_time = current_time;
+  // If no new audio samples have been received since last time, don't bother
+  // running the network model.
+  if (how_many_new_slices == 0) {
+    return;
+  }
+
+  // Run the model on the spectrogram input and make sure it succeeds.
+  TfLiteStatus invoke_status = interpreter->Invoke();
+  if (invoke_status != kTfLiteOk) {
+    error_reporter->Report("Invoke failed");
+    return;
+  }
+
+  // Obtain a pointer to the output tensor
+  TfLiteTensor* output = interpreter->output(0);
+  // Determine whether a command was recognized based on the output of inference
+  const char* found_command = nullptr;
+  uint8_t score = 0;
+  bool is_new_command = false;
+  TfLiteStatus process_status = recognizer->ProcessLatestResults(
+      output, current_time, &found_command, &score, &is_new_command);
+  if (process_status != kTfLiteOk) {
+    error_reporter->Report("RecognizeCommands::ProcessLatestResults() failed");
+    return;
+  }
+  // Do something based on the recognized command. The default implementation
+  // just prints to the error console, but you should replace this with your
+  // own function for a real application.
+  RespondToCommand(error_reporter, current_time, found_command, score,
+                   is_new_command);
+
+
+  }
+
+  // Plot the results in the LCD screen
+   LCD_Output(error_reporter, x_val, y_val);
 }
 
 /**
@@ -252,14 +300,14 @@ int main(void)
   
   if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
-    Error_Handler();
+    error_handler();
   }
 
   /** Activate the Over-Drive mode
   */
   if(HAL_PWREx_EnableOverDrive() != HAL_OK)
   {
-    Error_Handler();
+    error_handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
@@ -274,7 +322,7 @@ int main(void)
 
   if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_6) != HAL_OK)
   {
-    Error_Handler();
+    error_handler();
   }
 }
 
