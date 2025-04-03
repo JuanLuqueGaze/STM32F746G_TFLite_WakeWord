813ecd7787668da1b74311d11c84f391493ff8e1
JuanLuque <juanluquegiron12@gmail.com>
2025-04-03 09:39:11 +0200

    Changed the main.cpp for the tensorflow repository file

--- Core/main.cpp
+++ Core/main.cpp
@@ -1,35 +1,24 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
-#include "main.h"
-#include "string.h"
-#include "cmsis_os.h"
-//#include "fatfs.h"
-#include "lcd.h"
-//#include "usb_host.h"
-#include "stm32746g_discovery.h"
-//Includes for the tensorflow code
+/* Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+
 #include "audio_provider.h"
 #include "command_responder.h"
 #include "feature_provider.h"
-#include "micro_features_micro_model_settings.h"
-#include "micro_features_tiny_conv_micro_features_model_data.h"
+#include "micro_model_settings.h"
+#include "tiny_conv_micro_features_model_data.h"
 #include "recognize_commands.h"
 #include "tensorflow/lite/micro/kernels/micro_ops.h"
 #include "tensorflow/lite/micro/micro_error_reporter.h"
@@ -38,128 +27,26 @@
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/version.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
 // Globals, used for compatibility with Arduino-style sketches.
 namespace {
-  tflite::ErrorReporter* error_reporter = nullptr;
-  const tflite::Model* model = nullptr;
-  tflite::MicroInterpreter* interpreter = nullptr;
-  TfLiteTensor* model_input = nullptr;
-  FeatureProvider* feature_provider = nullptr;
-  RecognizeCommands* recognizer = nullptr;
-  int32_t previous_time = 0;
-  
-  // Create an area of memory to use for input, output, and intermediate arrays.
-  // The size of this will depend on the model you're using, and may need to be
-  // determined by experimentation.
-  constexpr int kTensorArenaSize = 10 * 1024;
-  uint8_t tensor_arena[kTensorArenaSize];
-  }  // namespace
+tflite::ErrorReporter* error_reporter = nullptr;
+const tflite::Model* model = nullptr;
+tflite::MicroInterpreter* interpreter = nullptr;
+TfLiteTensor* model_input = nullptr;
+FeatureProvider* feature_provider = nullptr;
+RecognizeCommands* recognizer = nullptr;
+int32_t previous_time = 0;
 
-	extern const float INPUT_RANGE = 2.f * 3.14159265359f;
+// Create an area of memory to use for input, output, and intermediate arrays.
+// The size of this will depend on the model you're using, and may need to be
+// determined by experimentation.
+constexpr int kTensorArenaSize = 10 * 1024;
+uint8_t tensor_arena[kTensorArenaSize];
+}  // namespace
 
-  UART_HandleTypeDef DebugUartHandler; //This was not declared before, i don't know if i missed it in a previous step
-
-
-#if defined ( __ICCARM__ ) /*!< IAR Compiler */
-#pragma location=0x2004c000
-ETH_DMADescTypeDef  DMARxDscrTab[ETH_RX_DESC_CNT]; /* Ethernet Rx DMA Descriptors */
-#pragma location=0x2004c0a0
-ETH_DMADescTypeDef  DMATxDscrTab[ETH_TX_DESC_CNT]; /* Ethernet Tx DMA Descriptors */
-
-#elif defined ( __CC_ARM )  /* MDK ARM Compiler */
-
-__attribute__((at(0x2004c000))) ETH_DMADescTypeDef  DMARxDscrTab[ETH_RX_DESC_CNT]; /* Ethernet Rx DMA Descriptors */
-__attribute__((at(0x2004c0a0))) ETH_DMADescTypeDef  DMATxDscrTab[ETH_TX_DESC_CNT]; /* Ethernet Tx DMA Descriptors */
-
-#elif defined ( __GNUC__ ) /* GNU Compiler */
-
-ETH_DMADescTypeDef DMARxDscrTab[ETH_RX_DESC_CNT] __attribute__((section(".RxDecripSection"))); /* Ethernet Rx DMA Descriptors */
-ETH_DMADescTypeDef DMATxDscrTab[ETH_TX_DESC_CNT] __attribute__((section(".TxDecripSection")));   /* Ethernet Tx DMA Descriptors */
-#endif
-
-ETH_TxPacketConfig TxConfig;
-
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-static void system_clock_config(void);
-static void cpu_cache_enable(void);
-static void error_handler(void);
-static void uart1_init(void);
-void handle_output(tflite::ErrorReporter* error_reporter, float x_value, float y_value);
-
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-  cpu_cache_enable();
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  system_clock_config();
-
-  BSP_LED_Init(LED_GREEN);
-
-  /* Configure the peripherals common clocks */
-  //PeriphCommonClock_Config();  //Maybe I should remove this line
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  uart1_init();
-
-  LCD_Init();
-
-    // Set up logging. Google style is to avoid globals or statics because of
+// The name of this function is important for Arduino compatibility.
+void setup() {
+  // Set up logging. Google style is to avoid globals or statics because of
   // lifetime uncertainty, but since this has a trivial destructor it's okay.
   // NOLINTNEXTLINE(runtime-global-variables)
   static tflite::MicroErrorReporter micro_error_reporter;
@@ -168,14 +55,13 @@ int main(void)
   // Map the model into a usable data structure. This doesn't involve any
   // copying or parsing, it's a very lightweight operation.
   model = tflite::GetModel(g_tiny_conv_micro_features_model_data);
-  if(model->version() != TFLITE_SCHEMA_VERSION)
- {
-   TF_LITE_REPORT_ERROR(error_reporter,
-                          "Model provided is schema version %d not equal "
-                          "to supported version %d.",
-                          model->version(), TFLITE_SCHEMA_VERSION);
-     return 0;
- }
+  if (model->version() != TFLITE_SCHEMA_VERSION) {
+    error_reporter->Report(
+        "Model provided is schema version %d not equal "
+        "to supported version %d.",
+        model->version(), TFLITE_SCHEMA_VERSION);
+    return;
+  }
 
   // Pull in only the operation implementations we need.
   // This relies on a complete list of all the ops needed by this graph.
@@ -203,12 +89,10 @@ int main(void)
 
   // Allocate memory from the tensor_arena for the model's tensors.
   TfLiteStatus allocate_status = interpreter->AllocateTensors();
-  if (allocate_status != kTfLiteOk)
-  {
-      TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
-      return 0;
+  if (allocate_status != kTfLiteOk) {
+    error_reporter->Report("AllocateTensors() failed");
+    return;
   }
-
 
   // Get information about the memory area to use for the model's input.
   model_input = interpreter->input(0);
@@ -217,7 +101,7 @@ int main(void)
       (model_input->dims->data[2] != kFeatureSliceSize) ||
       (model_input->type != kTfLiteUInt8)) {
     error_reporter->Report("Bad input tensor parameters in model");
-    return 0;
+    return;
   }
 
   // Prepare to access the audio spectrograms from a microphone or other source
@@ -231,30 +115,31 @@ int main(void)
   recognizer = &static_recognizer;
 
   previous_time = 0;
+}
 
-  while (1)
-  {
-      // Fetch the spectrogram for the current time.
+// The name of this function is important for Arduino compatibility.
+void loop() {
+  // Fetch the spectrogram for the current time.
   const int32_t current_time = LatestAudioTimestamp();
   int how_many_new_slices = 0;
   TfLiteStatus feature_status = feature_provider->PopulateFeatureData(
       error_reporter, previous_time, current_time, &how_many_new_slices);
   if (feature_status != kTfLiteOk) {
     error_reporter->Report("Feature generation failed");
-    return 0;
+    return;
   }
   previous_time = current_time;
   // If no new audio samples have been received since last time, don't bother
   // running the network model.
   if (how_many_new_slices == 0) {
-    return 0;
+    return;
   }
 
   // Run the model on the spectrogram input and make sure it succeeds.
   TfLiteStatus invoke_status = interpreter->Invoke();
   if (invoke_status != kTfLiteOk) {
     error_reporter->Report("Invoke failed");
-    return 0;
+    return;
   }
 
   // Obtain a pointer to the output tensor
@@ -267,148 +152,11 @@ int main(void)
       output, current_time, &found_command, &score, &is_new_command);
   if (process_status != kTfLiteOk) {
     error_reporter->Report("RecognizeCommands::ProcessLatestResults() failed");
-    return 0;
+    return;
   }
   // Do something based on the recognized command. The default implementation
   // just prints to the error console, but you should replace this with your
   // own function for a real application.
   RespondToCommand(error_reporter, current_time, found_command, score,
                    is_new_command);
-
-
-  }
-
 }
-
-
-
-void handle_output(tflite::ErrorReporter* error_reporter, float x_value, float y_value)
-{
-  // Log the current X and Y values
-  TF_LITE_REPORT_ERROR(error_reporter, "x_value: %f, y_value: %f\n", x_value, y_value);
-
-  // A custom function can be implemented and used here to do something with the x and y values.
-  // In my case I will be plotting sine wave on an LCD.
-  LCD_Output(x_value, y_value);
-}
-
-
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
- void system_clock_config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-    // Enable HSE Oscillator and activate PLL with HSE as source
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
-    RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLM       = 25;
-    RCC_OscInitStruct.PLL.PLLN       = 400;
-    RCC_OscInitStruct.PLL.PLLP       = RCC_PLLP_DIV2;
-    RCC_OscInitStruct.PLL.PLLQ       = 9;
-  
-  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    error_handler();
-  }
-
-  /** Activate the Over-Drive mode
-  */
-  if(HAL_PWREx_EnableOverDrive() != HAL_OK)
-  {
-    error_handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-
-    // Initializes the CPU, AHB and APB busses clocks
-  RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-
-  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_6) != HAL_OK)
-  {
-    error_handler();
-  }
-}
-
-
-static void uart1_init(void)
-{
-
-    /*##-1- Configure the UART peripheral ######################################*/
-  	/* Put the USART peripheral in the Asynchronous mode (UART Mode)
-  	   UART configured as follows:
-  	      - Word Length = 8 Bits
-  	      - Stop Bit = One Stop bit
-  	      - Parity = None
-  	      - BaudRate = 9600 baud
-  	      - Hardware flow control disabled (RTS and CTS signals)
-  	 */
-  
-     DebugUartHandler.Instance        = DISCOVERY_COM1;
-     DebugUartHandler.Init.BaudRate   = 9600;
-     DebugUartHandler.Init.WordLength = UART_WORDLENGTH_8B;
-     DebugUartHandler.Init.StopBits   = UART_STOPBITS_1;
-     DebugUartHandler.Init.Parity     = UART_PARITY_NONE;
-     DebugUartHandler.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
-     DebugUartHandler.Init.Mode       = UART_MODE_TX_RX;
-     DebugUartHandler.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-   
-     if(HAL_UART_DeInit(&DebugUartHandler) != HAL_OK)
-     {
-       error_handler();
-     }
-     if(HAL_UART_Init(&DebugUartHandler) != HAL_OK)
-     {
-         error_handler();
-     }
-
-}
-
-static void error_handler(void)
-  {
-      // Turn Green LED ON
-      BSP_LED_On(LED_GREEN);
-      while(1);
-  }
- 
-
-
-
-  static void cpu_cache_enable(void)
- {
-    // Enable I-Cache
-    SCB_EnableICache();
-    // Enable D-Cache
-    SCB_EnableDCache();
- }
- 
- #ifdef  USE_FULL_ASSERT
- /**
-   * @brief  Reports the name of the source file and the source line number
-   *         where the assert_param error has occurred.
-   * @param  file: pointer to the source file name
-   * @param  line: assert_param error line source number
-   * @retval None
-   */
- void assert_failed(uint8_t *file, uint32_t line)
- {
-   /* USER CODE BEGIN 6 */
-   /* User can add his own implementation to report the file name and line number,
-      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-   /* USER CODE END 6 */
- }
- #endif /* USE_FULL_ASSERT */
